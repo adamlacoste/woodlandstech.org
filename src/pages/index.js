6088ae1cc80cf276d531fa9b33b6e8111925ae55
Phil <phil@upvalue.io>
2018-06-22 19:39:01 -0500

    Invite

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,18 +19,10 @@ const IndexRoute = () => {
         </p>
 
         <h3>Join us on Slack</h3>
-        <form className="form-horizontal" name="slack" method="POST" netlify="true">
-          <div className="form-group">
-            <label className="form-label" for="email">Email</label>
-            &nbsp;
-            <div className="has-icon-left">
-              <input className="form-input" type="email" id="email" name="email" placeholder="name@example.com" />
-              <i className="form-icon icon icon-check" />
-            </div>
-            &nbsp;
-            <button className="btn btn-primary" type="submit">Get a Slack invite</button>
-          </div>
-        </form>
+
+        <a className="btn btn-primary" href="https://join.slack.com/t/notwoodlandstech/shared_invite/enQtMzg2NTYyMTQyMTAwLWUwODg1YzAxYjA0OTk4YTlmNjk3ZjRkZTFlMDRkY2ViZTc3N2QxMmU3MzE2YmM2MWNjZTI4Yzc3YjNkMjMwYjk">
+          Click here to join our Slack
+        </a>
 
         <h3>Join us on Meetup</h3>
         <iframe title="meetup"width="225" height="570" src="http://meetu.ps/3j0GH6" frameborder="0"></iframe>
